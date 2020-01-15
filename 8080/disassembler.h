9425a4ea944dffcd2cfec7152cc773649e630eba
Jonathan Simpson <jsimpson.github@gmail.com>
2020-01-14 20:39:57 -0800

    clean up

--- 8080/disassembler.h
+++ 8080/disassembler.h
@@ -5,6 +5,7 @@ typedef struct instruction
 {
     char size;
     char mnemonic[255];
+    char has_address;
 } instruction_t;
 
 int disassemble(unsigned char * buf, char * disassembled, int program_counter);
